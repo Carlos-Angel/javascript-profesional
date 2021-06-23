1911ac5a943e615f295118e11daadbc1b5d61da9
Charly_Angel <c.a.a.a.503@gmail.com>
2021-06-23 14:57:43 -0500

    feat(clase-23): service workers

--- assets/index.js
+++ assets/index.js
@@ -13,3 +13,9 @@ button.onclick = () => player.tooglePlay();
 
 const buttonMute = document.getElementById('muted');
 buttonMute.onclick = () => player.toogleMute();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch((error) => {
+    console.error(error.message);
+  });
+}
