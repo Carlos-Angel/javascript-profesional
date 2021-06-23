3c963b44043ed98ac21d5aba01e11e9a40475147
Charly_Angel <c.a.a.a.503@gmail.com>
2021-06-23 18:38:18 -0500

    feat(clase-29): convertir el proyecto a typescript parte 2

---
+++ assets/index.ts
@@ -0,0 +1,21 @@
+import MediaPlayer from './MediaPlayer';
+import AutoPlay from './plugins/AutoPlay';
+import AutoPause from './plugins/AutoPause';
+
+const video = document.querySelector('video');
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay(), new AutoPause()],
+});
+
+const playButton: HTMLElement = document.querySelector('#playButton');
+playButton.onclick = () => player.togglePlay();
+
+const muteButton: HTMLElement = document.querySelector('#muteButton');
+muteButton.onclick = () => player.toggleMute();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch((error) => {
+    console.error(error.message);
+  });
+}
